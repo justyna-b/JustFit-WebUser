736f1c8894a9aa7554c8b53d657faf7e1f4e9cb0
Justyna Brzezinska <brzezinska.j.uczelnia@gmail.com>
2020-04-16 13:51:51 +0200

    The basics of graphic module #SPRINT3

--- src/components/programme/ProgrammeView.js
+++ src/components/programme/ProgrammeView.js
@@ -1,11 +1,72 @@
 import React from 'react'
+import AuthService from '../logging/AuthService'
+import { Redirect } from 'react-router-dom'
+import HeaderPanel from '../navigation/HeaderPanel.js'
+import Footer from '../home/Footer.js'
 
-function ProgrammeView () {
-  return (
-    <div>
-      Programme
-    </div>
-  )
+class ProgrammeView extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      auth: true,
+      name: '',
+      surname: '',
+      email: '',
+      id: '',
+      activites: [],
+      activityId: ''
+    }
+    this.Auth = new AuthService()
+  }
+
+  async componentDidMount () {
+    if (this.Auth.loggedIn()) {
+      await this.Auth.fetch(
+        'https://justfitclient.pythonanywhere.com/account/client/properties/'
+      )
+        .then(res => {
+          this.setState({
+            name: res.first_name,
+            surname: res.last_name,
+            email: res.email,
+            phoneNumber: res.phone_number,
+            id: res.id
+          })
+        })
+        .catch(error => {
+          console.log({ message: 'ERROR ' + error })
+        })
+    } else {
+      this.setState({ auth: false })
+    }
+
+    await this.Auth.fetch('http://localhost:8090//getAllActivityTypes').then(
+      res => {
+        this.setState({
+          activities: res
+        })
+      }
+    )
+  }
+  render () {
+    return (
+      <div className='App'>
+        {this.state.auth ? '' : <Redirect to='/login' />}
+        <header>
+          <HeaderPanel />
+        </header>
+        <body className='App-Body'>
+          Zajęcia:
+          {this.state.activites.map(activity => (
+            <p>{activity}</p>
+          ))}
+        </body>
+        <footer style={{ backgroundColor: 'black' }}>
+          <Footer />
+        </footer>
+      </div>
+    )
+  }
 }
 
 export default ProgrammeView
