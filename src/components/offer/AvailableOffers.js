da0eefd3119e9c5e75b69950c6e1a9e6712d8ba7
Justyna Brzezinska <brzezinska.j.uczelnia@gmail.com>
2020-05-13 09:43:53 +0200

    View of users offer in case he does not own any carnet (card: info that there is no carnet and link to view where is possibility to buy it). Loading screen added till data is not fetched.

--- src/components/offer/AvailableOffers.js
+++ src/components/offer/AvailableOffers.js
@@ -6,6 +6,7 @@ import Footer from '../home/Footer.js'
 import CardHeader from './OffersCardHeader.js'
 import CardBody from './OffersCardBody.js'
 import '../../styles/OffersCard.css'
+import LoadingScreen from 'react-loading-screen'
 
 class AvailableOffers extends React.Component {
   constructor (props) {
@@ -17,7 +18,8 @@ class AvailableOffers extends React.Component {
       email: '',
       id: '',
       offers: [],
-      offerValuePurchaseId: ''
+      offerValuePurchaseId: '',
+      loading: true
     }
     this.Auth = new AuthService()
   }
@@ -48,17 +50,17 @@ class AvailableOffers extends React.Component {
       this.setState({
         offers: res
       })
-    })
+    }).then(this.setState({loading: false}))
   }
 
   submitHandler = event => {
     this.setState({ offerValuePurchaseId: event.target.value })
-
+    console.log('purchase id' + this.state.offerValuePurchaseId)
     event.preventDefault()
     this.Auth.fetch('https://justfitclient.pythonanywhere.com/api/product/', {
       method: 'POST',
       body: JSON.stringify({
-        id: this.state.offerValuePurchaseId
+        id_product: this.state.offerValuePurchaseId
       })
     }).then(response => {
       if (response.status >= 200 && response.status < 300) {
@@ -70,33 +72,44 @@ class AvailableOffers extends React.Component {
 
   render () {
     return (
+        <LoadingScreen
+            loading={this.state.loading}
+            bgColor='grey'
+            spinnerColor='orange'
+            textColor='orange'
+            // logoSrc='/logo.png'
+            text='Zmieniaj się z nami'
+          >
       <div className='App'>
         {this.state.auth ? '' : <Redirect to='/login' />}
         <header>
           <HeaderPanel />
         </header>
         <body className='App-Body'>
-          {this.state.offers.map(offer => (
-            <article className='card' key={offer.id}>
-              <CardHeader
-                image={
-                  'https://cdn.pixabay.com/photo/2015/06/24/14/41/weights-820144_1280.jpg'
-                }
-              />
-              <CardBody
-                title={offer.name}
-                text={offer.description}
-                duration={offer.durationInMonths}
-                value={offer.id}
-                onClick={this.submitHandler}
-              />
-            </article>
-          ))}
+        
+            {this.state.offers.map(offer => (
+              <article className='card' key={offer.id}>
+                <CardHeader
+                  image={
+                    'https://cdn.pixabay.com/photo/2015/06/24/14/41/weights-820144_1280.jpg'
+                  }
+                />
+                <CardBody
+                  title={offer.name}
+                  text={offer.description}
+                  duration={offer.durationInMonths}
+                  value={offer.id}
+                  onClick={this.submitHandler}
+                />
+              </article>
+            ))}
         </body>
         <footer style={{ backgroundColor: 'black' }}>
           <Footer />
         </footer>
       </div>
+                </LoadingScreen>
+
     )
   }
 }
